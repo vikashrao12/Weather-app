412a2a76c4f20ec1bc9c650b7c6f64b767cdfbe8
vikashrao12 <vikashrao12@gmail.com>
2026-01-20 13:25:01 +0530

    Connected JavaScript file and selected DOM elements

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Input field
+const cityInput = document.querySelector("input");
+
+// Buttons
+const buttons = document.querySelectorAll("button");
+const searchBtn = buttons[0];
+const locationBtn = buttons[1];
+
+// Error message 
+const errorMessage = document.querySelector("section p");
+
+// Recent search 
+const recentDropdown = document.querySelector("select");
+
+// confirm 
+console.log("JS loaded");
